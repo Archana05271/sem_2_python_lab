72443c37ae989ba1bd3e6309df8e553a191c4ef5
Archana05271 <achu68352@gmail.com>
2024-11-29 09:12:03 +0530

    fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,12 +1,9 @@
 n = int(input("Enter the number:"))
 a = 0
 b = 1
-next_no = b  
-count = 1
-
-while count <= n:
-    print(next_no, end=" ")
-    count += 1
-    a, b = b, next_no
-    next_no = a + b
-print()
+print("The Fibonacci Series is:")
+for i in range(n):
+    print(a)
+    c=a+b
+    a=b
+    b=c
