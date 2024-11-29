93b093d4a85a77e5b2ca0df82564e875e7fd6db5
Archana05271 <achu68352@gmail.com>
2024-11-29 09:09:44 +0530

    Area.py

--- Area.py
+++ Area.py
@@ -2,19 +2,19 @@
 l=int(input("Enter the length: "))
 b=int(input("Enter the breadth: "))
 Area=l*b
-print("Area of a rectangle:",Area)
+print("Area of  rectangle:",Area)
 #square
 side=int(input("Enter the side: "))
 Area=side*side
-print("Area of a Square: ",Area)
+print("Area of  Square: ",Area)
 #circle
 radius=int(input("Enter the radius: "))
 Area=3.14*(radius*radius)
-print("Area of a circle: ",Area)
+print("Area of  circle: ",Area)
 #triangle
 base=int(input("Enter the base: "))
 height=int(input("Enter the height: "))
 Area=1/2*base*height
-print("Area of a triangle: ",Area)
+print("Area of  triangle: ",Area)
 
 
